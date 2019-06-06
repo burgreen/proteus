93dc5558b128531b03c8a70b516500617314498d
Greg Burgreen <greg.burgreen@msstate.edu>
2019-06-06 10:35:44 -0500

    tweak to Kappa and Dissipation .py

--- proteus/mprans/Dissipation.py
+++ proteus/mprans/Dissipation.py
@@ -297,11 +297,11 @@ class Coefficients(proteus.TransportCoefficients.TC_base):
             self.q_phi = None
             self.ebq_phi = None
             self.ebqe_phi = None
-        # msu-turb end
-        else:
-            self.q_phi =-numpy.ones( modelList[self.kappa_modelIndex].q[('u', 0)].shape, 'd')
-            #self.ebq_phi =-numpy.ones( modelList[self.dissipation_modelIndex].ebq[('u', 0)].shape, 'd')
-            self.ebqe_phi = -numpy.ones( modelList[self.kappa_modelIndex].ebqe[('u', 0)].shape, 'd')
+        #else:
+        #    self.q_phi =-numpy.ones( modelList[self.kappa_modelIndex].q[('u', 0)].shape, 'd')
+        #    #self.ebq_phi =-numpy.ones( modelList[self.dissipation_modelIndex].ebq[('u', 0)].shape, 'd')
+        #    self.ebqe_phi = -numpy.ones( modelList[self.kappa_modelIndex].ebqe[('u', 0)].shape, 'd')
+        ## msu-turb end
         # flow model
         self.u_old_dof = numpy.copy(self.model.u[0].dof)
 
